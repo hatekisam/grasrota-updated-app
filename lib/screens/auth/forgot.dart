38978f2c0e166152f72ac89a0b6fdce502435679
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-21 10:27:49 +0200

    created the forgot page

--- lib/screens/auth/forgot.dart
+++ lib/screens/auth/forgot.dart
@@ -21,240 +21,121 @@ class _ForgotState extends State<Forgot> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-            child: Form(
-              key: _formKey,
-              child: Column(
+    return loading
+        ? Loading()
+        : WillPopScope(
+            onWillPop: () {
+              setState(() {
+                widget.toggleView(1);
+              });
+              return new Future(() => true);
+            },
+            child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  FractionallySizedBox(
-                    widthFactor: 1,
-                    child: Image.asset("assets/logo.png"),
-                  ),
-                  // const SizedBox(height: 20),
-                  MediaQuery(
-                    data: MediaQuery.of(context).copyWith(
-                      textScaleFactor: 1.0,
-                    ),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          textInputAction: TextInputAction.next,
-
-                          //   decoration:
-                          //       textInputDecoration.copyWith(hintText: "Email"),
-                          validator: (val) =>
-                              val!.isEmpty ? "Skriv inn en email" : null,
-                          onChanged: (val) {
-                            setState(() => email = val);
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
-                          //   decoration:
-                          //       textInputDecoration.copyWith(hintText: "Passord"),
-                          obscureText: true,
-                          validator: (val) => val!.length < 6
-                              ? "Skriv inn et passord på 6+ tegn"
-                              : null,
-                          onChanged: (val) {
-                            setState(() => password = val);
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  InkWell(
-                    onTap: () {
-                      HapticFeedback.heavyImpact();
-                      //       Navigator.push(
-                      //         context,
-                      //         PageRouteBuilder(
-                      //             pageBuilder: (context, animation1, animation2) =>
-                      //                 Forgot(toggleView: widget.toggleView)),
-                      //       );
-                    },
-                    child: const Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Text(
-                        "glemt passord?",
-                        textScaleFactor: 1.0,
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 50),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          //Image.asset("assets/logo.png", width: 100),
+                          const SizedBox(height: 20),
+                          MediaQuery(
+                            data: MediaQuery.of(context).copyWith(
+                              textScaleFactor: 1.0,
+                            ),
+                            child: TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: textInputDecoration.copyWith(
+                                  hintText: "Email"),
+                              validator: (val) =>
+                                  val!.isEmpty ? "Skriv inn en email" : null,
+                              onChanged: (val) {
+                                setState(() => email = val);
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          InkWell(
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: 50,
+                                width: 225,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(40),
+                                  gradient: const LinearGradient(
+                                      colors: [Colors.cyan, Colors.greenAccent],
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight),
+                                ),
+                                child: Text("Nullstill",
+                                    textScaleFactor: 1.0,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16)),
+                              ),
+                              onTap: () async {
+                                HapticFeedback.heavyImpact();
+                                if (_formKey.currentState!.validate()) {
+                                  setState(() => loading = true);
+                                  dynamic result =
+                                      await _auth.resetPassword(email);
+                                  if (result == null) {
+                                    setState(() {
+                                      error = "Sjekk mailen din!";
+                                      loading = false;
+                                    });
+                                  }
+                                }
+                              }),
+                          const SizedBox(height: 20),
+                          Text(error,
+                              style: const TextStyle(
+                                  color: Colors.green, fontSize: 16),
+                              textAlign: TextAlign.center),
+                          const SizedBox(height: 20),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                const TextSpan(
+                                  text: "husker passord? ",
+                                  style: const TextStyle(color: Colors.black),
+                                ),
+                                TextSpan(
+                                  text: "logg inn",
+                                  style: const TextStyle(
+                                      color: Colors.cyan,
+                                      fontWeight: FontWeight.bold,
+                                      height: 2.5),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      HapticFeedback.heavyImpact();
+                                      Navigator.push(
+                                        context,
+                                        PageRouteBuilder(
+                                            pageBuilder: (context, animation1,
+                                                    animation2) =>
+                                                SignIn(
+                                                    toggleView:
+                                                        widget.toggleView)),
+                                      );
+                                    },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  InkWell(
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 50,
-                        width: 225,
-                        color: Color(0xFF0acbee),
-                        child: const Text("Logg inn",
-                            textScaleFactor: 1.0,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16)),
-                      ),
-                      onTap: () async {
-                        HapticFeedback.heavyImpact();
-                        // if (_formKey.currentState!.validate()) {
-                        //   setState(() => loading = true);
-                        //   dynamic result =
-                        //       await _auth.ForgotWithEmailAndPassword(
-                        //           email.trim(), password.trim(), false);
-                        //   debugPrint('@@@@@@@@@@@@@@@');
-                        //   print(result);
-                        //   debugPrint('@@@@@@@@@@@@@@@98258879');
-                        //   if (result == null) {
-                        //     setState(() {
-                        //       error = "feil email eller passord";
-                        //       loading = false;
-                        //     });
-                        //   } else if (result is String) {
-                        //     setState(() {
-                        //       error = result;
-                        //       loading = false;
-                        //     });
-                        //   } else {
-                        //     print('object');
-                        //     Navigator.pushReplacement(
-                        //       context,
-                        //       PageRouteBuilder(
-                        //         pageBuilder:
-                        //             (context, animation1, animation2) =>
-                        //                 Wrapper(),
-                        //         transitionDuration: Duration(seconds: 0),
-                        //       ),
-                        //     );
-                        //   }
-                        // }
-                      }),
-                  const SizedBox(height: 20),
-                  Text(error,
-                      textScaleFactor: 1.0,
-                      style: const TextStyle(color: Colors.red, fontSize: 16),
-                      textAlign: TextAlign.center),
-                  const SizedBox(height: 20),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: "har ikke en konto? ",
-                          style: const TextStyle(color: Colors.black),
-                        ),
-                        TextSpan(
-                          text: "lag en konto",
-                          style: const TextStyle(
-                              color: Colors.cyan,
-                              fontWeight: FontWeight.bold,
-                              height: 2.5),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              HapticFeedback.heavyImpact();
-                              //       Navigator.push(
-                              //         context,
-                              //         PageRouteBuilder(
-                              //             pageBuilder:
-                              //                 (context, animation1, animation2) =>
-                              //                     Register(
-                              //                         toggleView: widget.toggleView)),
-                              //       );
-
-                              // widget.toggleView(3);
-                            },
-                        ),
-                      ],
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          //   if (Platform.isAndroid) ...[
-          //     MediaQuery(
-          //       data: MediaQuery.of(context).copyWith(
-          //         textScaleFactor: 1.0,
-          //       ),
-          //       child: ForgotButton(Buttons.Google, text: "Logg inn med Google",
-          //           onPressed: () async {
-          //         setState(() => loading = true);
-          //         StoredData.choosenOrg = "";
-          //         StoredData.adminOrg = "";
-          //         StoredData.isAdmin = false;
-          //         dynamic result = await _auth.ForgotWithGoogle();
-          //         print(result);
-
-          //         if (result == null) {
-          //           setState(() {
-          //             error = "noe gikk galt";
-          //             loading = false;
-          //           });
-          //         } else if (result is String) {
-          //           setState(() {
-          //             error = result;
-          //             loading = false;
-          //           });
-          //         } else {
-          //           print('object---------------');
-          //           // print(result);
-          //           print('User UID: ${result.user.uid}');
-          //           Navigator.pushReplacement(
-          //             context,
-          //             PageRouteBuilder(
-          //               pageBuilder: (context, animation1, animation2) =>
-          //                   Wrapper(),
-          //               transitionDuration: Duration(seconds: 0),
-          //             ),
-          //           );
-          //         }
-          //       }),
-          //     ),
-          //   ],
-          //   if (Platform.isIOS) ...[
-          //     MediaQuery(
-          //       data: MediaQuery.of(context).copyWith(
-          //         textScaleFactor: 1.0,
-          //       ),
-          //       child: ForgotButton(Buttons.Apple, text: "Logg inn med Apple",
-          //           onPressed: () async {
-          //         setState(() => loading = true);
-          //         StoredData.choosenOrg = "";
-          //         StoredData.adminOrg = "";
-          //         StoredData.isAdmin = false;
-          //         dynamic result = await _auth.ForgotWithApple();
-          //         if (result == null) {
-          //           setState(() {
-          //             error = "noe gikk galt";
-          //             loading = false;
-          //           });
-          //         } else if (result is String) {
-          //           setState(() {
-          //             error = result;
-          //             loading = false;
-          //           });
-          //         } else {
-          //           Navigator.pushReplacement(
-          //             context,
-          //             PageRouteBuilder(
-          //               pageBuilder: (context, animation1, animation2) =>
-          //                   Wrapper(),
-          //               transitionDuration: Duration(seconds: 0),
-          //             ),
-          //           );
-          //         }
-          //       }),
-          //     ),
-          //   ],
-        ],
-      ),
-    );
+          );
   }
 }
