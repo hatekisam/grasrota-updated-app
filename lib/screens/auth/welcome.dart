50281d1125246231b22e66f61406175bb8270162
Mugisha Yves <mugishayves189000@gmail.com>
2023-10-21 10:28:46 +0200

    convert the welcome page

--- lib/screens/auth/welcome.dart
+++ lib/screens/auth/welcome.dart
@@ -10,246 +10,90 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
-  int showSignIn = 4;
-  void toggleView(int val) {
-    setState(() => showSignIn = val);
-  }
+  final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+  // text field state
+  String email = "";
+  String password = "";
+  String error = "";
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-            child: Form(
-              key: _formKey,
-              child: Column(
+    return loading
+        ? Loading()
+        : WillPopScope(
+            // ignore: missing_return
+            onWillPop: () {
+              return widget.toggleView(0);
+            },
+            child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  FractionallySizedBox(
-                    widthFactor: 1,
-                    child: Image.asset("assets/logo.png"),
-                  ),
-                  // const SizedBox(height: 20),
-                  MediaQuery(
-                    data: MediaQuery.of(context).copyWith(
-                      textScaleFactor: 1.0,
-                    ),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          textInputAction: TextInputAction.next,
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 20, horizontal: 50),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          FractionallySizedBox(
+                            widthFactor: 1,
+                            child: Image.asset("assets/logo2.png"),
+                          ),
+                          // const SizedBox(height: 20),
+                          const SizedBox(height: 20),
+                          const Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Center(
+                              child: const Text(
+                                "Velkommen til Grasrota",
+                                textScaleFactor: 1.0,
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 20),
+                              ),
+                            ),
+                          ),
 
-                          //   decoration:
-                          //       textInputDecoration.copyWith(hintText: "Email"),
-                          validator: (val) =>
-                              val!.isEmpty ? "Skriv inn en email" : null,
-                          onChanged: (val) {
-                            setState(() => email = val);
-                          },
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
-                          //   decoration:
-                          //       textInputDecoration.copyWith(hintText: "Passord"),
-                          obscureText: true,
-                          validator: (val) => val!.length < 6
-                              ? "Skriv inn et passord på 6+ tegn"
-                              : null,
-                          onChanged: (val) {
-                            setState(() => password = val);
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  InkWell(
-                    onTap: () {
-                      HapticFeedback.heavyImpact();
-                      //       Navigator.push(
-                      //         context,
-                      //         PageRouteBuilder(
-                      //             pageBuilder: (context, animation1, animation2) =>
-                      //                 Forgot(toggleView: widget.toggleView)),
-                      //       );
-                    },
-                    child: const Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Text(
-                        "glemt passord?",
-                        textScaleFactor: 1.0,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  InkWell(
-                      child: Container(
-                        alignment: Alignment.center,
-                        height: 50,
-                        width: 225,
-                        color: Color(0xFF0acbee),
-                        child: const Text("Logg inn",
-                            textScaleFactor: 1.0,
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16)),
-                      ),
-                      onTap: () async {
-                        HapticFeedback.heavyImpact();
-                        // if (_formKey.currentState!.validate()) {
-                        //   setState(() => loading = true);
-                        //   dynamic result =
-                        //       await _auth.WelcomeWithEmailAndPassword(
-                        //           email.trim(), password.trim(), false);
-                        //   debugPrint('@@@@@@@@@@@@@@@');
-                        //   print(result);
-                        //   debugPrint('@@@@@@@@@@@@@@@98258879');
-                        //   if (result == null) {
-                        //     setState(() {
-                        //       error = "feil email eller passord";
-                        //       loading = false;
-                        //     });
-                        //   } else if (result is String) {
-                        //     setState(() {
-                        //       error = result;
-                        //       loading = false;
-                        //     });
-                        //   } else {
-                        //     print('object');
-                        //     Navigator.pushReplacement(
-                        //       context,
-                        //       PageRouteBuilder(
-                        //         pageBuilder:
-                        //             (context, animation1, animation2) =>
-                        //                 Wrapper(),
-                        //         transitionDuration: Duration(seconds: 0),
-                        //       ),
-                        //     );
-                        //   }
-                        // }
-                      }),
-                  const SizedBox(height: 20),
-                  Text(error,
-                      textScaleFactor: 1.0,
-                      style: const TextStyle(color: Colors.red, fontSize: 16),
-                      textAlign: TextAlign.center),
-                  const SizedBox(height: 20),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: "har ikke en konto? ",
-                          style: const TextStyle(color: Colors.black),
-                        ),
-                        TextSpan(
-                          text: "lag en konto",
-                          style: const TextStyle(
-                              color: Colors.cyan,
-                              fontWeight: FontWeight.bold,
-                              height: 2.5),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
+                          Align(
+                            child: const Text(
+                              "appen som gjør det enkelt og gøy å delta i ukentlig quiz konkurranse!",
+                              textScaleFactor: 1.0,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 16),
+                            ),
+                          ),
+                          const SizedBox(height: 60),
+                          MyButton(
+                            text: "Kom i gang",
+                            onTap: () async {
                               HapticFeedback.heavyImpact();
-                              //       Navigator.push(
-                              //         context,
-                              //         PageRouteBuilder(
-                              //             pageBuilder:
-                              //                 (context, animation1, animation2) =>
-                              //                     Register(
-                              //                         toggleView: widget.toggleView)),
-                              //       );
-
-                              // widget.toggleView(3);
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        SignIn(toggleView: widget.toggleView)),
+                              );
+                              //widget.toggleView(1);
                             },
-                        ),
-                      ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          //   if (Platform.isAndroid) ...[
-          //     MediaQuery(
-          //       data: MediaQuery.of(context).copyWith(
-          //         textScaleFactor: 1.0,
-          //       ),
-          //       child: WelcomeButton(Buttons.Google, text: "Logg inn med Google",
-          //           onPressed: () async {
-          //         setState(() => loading = true);
-          //         StoredData.choosenOrg = "";
-          //         StoredData.adminOrg = "";
-          //         StoredData.isAdmin = false;
-          //         dynamic result = await _auth.WelcomeWithGoogle();
-          //         print(result);
-
-          //         if (result == null) {
-          //           setState(() {
-          //             error = "noe gikk galt";
-          //             loading = false;
-          //           });
-          //         } else if (result is String) {
-          //           setState(() {
-          //             error = result;
-          //             loading = false;
-          //           });
-          //         } else {
-          //           print('object---------------');
-          //           // print(result);
-          //           print('User UID: ${result.user.uid}');
-          //           Navigator.pushReplacement(
-          //             context,
-          //             PageRouteBuilder(
-          //               pageBuilder: (context, animation1, animation2) =>
-          //                   Wrapper(),
-          //               transitionDuration: Duration(seconds: 0),
-          //             ),
-          //           );
-          //         }
-          //       }),
-          //     ),
-          //   ],
-          //   if (Platform.isIOS) ...[
-          //     MediaQuery(
-          //       data: MediaQuery.of(context).copyWith(
-          //         textScaleFactor: 1.0,
-          //       ),
-          //       child: WelcomeButton(Buttons.Apple, text: "Logg inn med Apple",
-          //           onPressed: () async {
-          //         setState(() => loading = true);
-          //         StoredData.choosenOrg = "";
-          //         StoredData.adminOrg = "";
-          //         StoredData.isAdmin = false;
-          //         dynamic result = await _auth.WelcomeWithApple();
-          //         if (result == null) {
-          //           setState(() {
-          //             error = "noe gikk galt";
-          //             loading = false;
-          //           });
-          //         } else if (result is String) {
-          //           setState(() {
-          //             error = result;
-          //             loading = false;
-          //           });
-          //         } else {
-          //           Navigator.pushReplacement(
-          //             context,
-          //             PageRouteBuilder(
-          //               pageBuilder: (context, animation1, animation2) =>
-          //                   Wrapper(),
-          //               transitionDuration: Duration(seconds: 0),
-          //             ),
-          //           );
-          //         }
-          //       }),
-          //     ),
-          //   ],
-        ],
-      ),
-    );
+          );
   }
 }
